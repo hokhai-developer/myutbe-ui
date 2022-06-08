0a29a62dec74900f2a6a6b4d273ed36b22462660
hokhai846 <hokhai846@gmail.com>
2022-06-08 19:50:08 +0700

    chỉnh lại scss và đã dùng được oke

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Header.module.scss';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Header = (props) => {
-  return <div>Header</div>;
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <p>logo</p>
+        <p>search</p>
+        <p>actions</p>
+      </div>
+    </div>
+  );
 };
 
 Header.propTypes = {};
