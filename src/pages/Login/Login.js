ebd2b90011919738f12536cde8fd50dd039cf6ad
hokhai846 <hokhai846@gmail.com>
2022-06-14 21:16:09 +0700

    quay lai trng cu khi thoat khoi form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import styles from './Login.module.scss';
-import Form from '~/components/Form';
+import { Form } from '~/components/Form';
 
 const cx = classNames.bind(styles);
 const Login = (props) => {
